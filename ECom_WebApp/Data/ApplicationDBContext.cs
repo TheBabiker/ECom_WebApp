fefe27125deeec2121fffe0777eaa148f8195852
Babiker <babikay.25@gmail.com>
2023-06-19 23:42:23 +0200

    DB Setup

---
+++ ECom_WebApp/Data/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using ECom_WebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECom_WebApp.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+            public DbSet<Category> Categories{ get; set; }
+    }
+}
