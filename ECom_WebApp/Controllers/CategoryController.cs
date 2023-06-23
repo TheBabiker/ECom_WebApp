79454ca595acd1316e2ae3f114a984b2b6bd9eec
Babiker <babikay.25@gmail.com>
2023-06-23 02:23:30 +0200

    Category  & Create category HTML

---
+++ ECom_WebApp/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using ECom_WebApp.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECom_WebApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDBContext _DbContext;
+        public CategoryController(ApplicationDBContext DbContext)
+        {
+            _DbContext = DbContext;
+        }
+        public IActionResult Index()
+        {
+            var objCategoryList = _DbContext.Categories.ToList();
+            return View(objCategoryList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
--- ECom_WebApp/Migrations/20230621183727_SeedCategoryTable.Designer.cs
+++ ECom_WebApp/Migrations/20230621183727_SeedCategoryTable.Designer.cs
@@ -3,6 +3,7 @@ using ECom_WebApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ECom_WebApp.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20230621183727_SeedCategoryTable")]
+    partial class SeedCategoryTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
